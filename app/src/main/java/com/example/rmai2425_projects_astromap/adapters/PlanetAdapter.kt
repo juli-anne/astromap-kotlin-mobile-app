a860f876cdd3d920dc15f5975dec1b094a6d71a9
Lana <lgolubic22@student.foi.hr>
2025-05-10 19:04:58 +0200

    SCRUM-8: Izrada korisničkog sučelja za edukativne module (SCRUM-37: Dizajniran XML layout za prikaz planeta putem Recycler Viewa).

---
+++ app/src/main/java/com/example/rmai2425_projects_astromap/adapters/PlanetAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.rmai2425_projects_astromap.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rmai2425_projects_astromap.R
+import com.example.rmai2425_projects_astromap.database.Planet
+
+class PlanetAdapter(private val planets: List<Planet>) : RecyclerView.Adapter<PlanetAdapter.MyViewHolder>() {
+
+    private val uniquePlanets = planets.distinctBy { it.id }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.title)
+        val planetImg: ImageView = view.findViewById(R.id.planet_img)
+        val menuIcon: ImageView = view.findViewById(R.id.menu_icon)
+        val planetInfo: TextView = view.findViewById(R.id.planetinfo)
+        val diameterIcon: ImageView = view.findViewById(R.id.diameter_icon)
+        val planetDiameter: TextView = view.findViewById(R.id.planet_diameter)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = uniquePlanets.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val planet = uniquePlanets[position]
+        holder.title.text = planet.ime
+        holder.planetInfo.text = planet.kratkiOpis
+        holder.planetDiameter.text = "${planet.promjer} km"
+
+        val dummyImage = when (planet.ime.trim().lowercase()) {
+            "merkur" -> R.drawable.mercury
+            "venera" -> R.drawable.venus
+            "zemlja" -> R.drawable.earth
+            "mars" -> R.drawable.mars
+            "jupiter" -> R.drawable.jupiter
+            "saturn" -> R.drawable.saturn
+            "uran" -> R.drawable.uranus
+            "neptun" -> R.drawable.neptune
+            else -> R.drawable.earth
+        }
+
+        holder.planetImg.setImageResource(dummyImage)
+    }
+}
--- app/src/main/java/com/example/rmai2425_projects_astromap/fragments/PlanetsFragment.kt
+++ app/src/main/java/com/example/rmai2425_projects_astromap/fragments/PlanetsFragment.kt
@@ -1,20 +1,71 @@
 package com.example.rmai2425_projects_astromap.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rmai2425_projects_astromap.R
+import com.example.rmai2425_projects_astromap.adapters.PlanetAdapter
+import com.example.rmai2425_projects_astromap.database.DatabaseProvider
+import com.example.rmai2425_projects_astromap.database.Planet
+import kotlinx.coroutines.launch
 
 class PlanetsFragment : Fragment() {
+
+    private lateinit var planetAdapter: PlanetAdapter
+    private lateinit var recyclerView: RecyclerView
+    private val planetsList: MutableList<Planet> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planets, container, false)
-    }
 
+        val rootView = inflater.inflate(R.layout.fragment_planets, container, false)
+
+        recyclerView = rootView.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+
+        val db = DatabaseProvider.getDatabase(requireContext())
+        val dao = db.entitiesDao()
+
+        lifecycleScope.launch {
+
+            planetsList.clear()
+
+            val planets = dao.getAllPlanets()
+
+            val uniquePlanets = planets.distinctBy { it.id }
+
+
+            planetsList.addAll(uniquePlanets)
+
+
+            planetAdapter = PlanetAdapter(planetsList)
+            recyclerView.adapter = planetAdapter
+        }
+
+        return rootView
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
